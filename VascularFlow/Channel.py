5b7743a6117d82875f6bdf4cce215f62e623707b
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2024-01-29 11:54:01 +0100

    ENH: Added skeleton module for channel flow

---
+++ VascularFlow/Channel.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def flow(flow_rate, radius, length, viscosity):
+    """
+    Calculates the pressure drop across a pipe of given radius and length
+    for a given flow rate and viscosity.
+
+    Parameters
+    ----------
+    flow_rate : float
+        The flow rate of the fluid through the pipe.
+    radius : np.ndarray
+        The radius of the pipe as a function of position along the pipe.
+    length : float
+        The length of the pipe.
+    viscosity : float
+        The viscosity of the fluid.
+
+    Returns
+    -------
+    pressure_drop : float
+        The pressure drop across the pipe as a function of position along the pipe.
+    """
+    pass

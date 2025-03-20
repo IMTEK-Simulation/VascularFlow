32ebfa46222af2aae1a8651460e76ffe39edfa8a
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2025-03-20 08:41:07 +0100

    TST: Added assembly test for 3x3 matrices

--- tests/test_assembly.py
+++ tests/test_assembly.py
@@ -5,7 +5,7 @@ from VascularFlow.Numerics.BasisFunctions import LinearBasis
 from VascularFlow.Numerics.ElementMatrices import first_first
 
 
-def test_assemble_system_matrix():
+def test_assemble_2x2():
     element_matrices_nn = first_first(1, LinearBasis())
 
     np.testing.assert_allclose(element_matrices_nn, [[1, -1], [-1, 1]])
@@ -21,5 +21,30 @@ def test_assemble_system_matrix():
     np.testing.assert_allclose(
         system_matrix_gg,
         [[1, -1, 0, 0], [-1, 2, -1, 0], [0, -1, 2, -1], [0, 0, -1, 1]],
+        atol=1e-6,
+    )
+
+
+def test_assemble_3x3():
+    element_matrices_nn = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
+    element_matrices_enn = element_matrices_nn.reshape((1, 3, 3)) * np.ones(3).reshape(
+        (3, 1, 1)
+    )
+
+    system_matrix_gg = assemble_system_matrix(element_matrices_enn)
+
+    assert system_matrix_gg.shape == (7, 7)
+
+    np.testing.assert_allclose(
+        system_matrix_gg,
+        [
+            [1, 1, 1, 0, 0, 0, 0],
+            [1, 1, 1, 0, 0, 0, 0],
+            [1, 1, 2, 1, 1, 0, 0],
+            [0, 0, 1, 1, 1, 0, 0],
+            [0, 0, 1, 1, 2, 1, 1],
+            [0, 0, 0, 0, 1, 1, 1],
+            [0, 0, 0, 0, 1, 1, 1],
+        ],
         atol=1e-6,
     )

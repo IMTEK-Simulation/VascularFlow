e10b47a9e47d78110a9bab17f0e081dd5d18b41c
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2025-03-20 08:54:23 +0100

    ENH: Clamped boundary conditions for beam; throws singular matrix for large number of grid points

--- VascularFlow/Elasticity/Beam.py
+++ VascularFlow/Elasticity/Beam.py
@@ -25,13 +25,39 @@ def euler_bernoulli(x_n, q_g):
     element_matrix_nn = second_second(1, QuadraticBasis())
     nb_nodes, _ = element_matrix_nn.shape
 
+    # Compute element matrices
     dx_e = x_n[1:] - x_n[:-1]  # Width of each element
     element_matrices_enn = element_matrix_nn.reshape((1, nb_nodes, nb_nodes)) * (
         dx_e ** (-3)
     ).reshape(-1, 1, 1)
 
+    # Assemble system matrix
     system_matrix_gg = assemble_system_matrix(element_matrices_enn)
 
+    # Add boundary conditions
+    # Fixed displacement left
+    system_matrix_gg[0] = 0
+    system_matrix_gg[0, 0] = 1
+    q_g[0] = 0
+    # Fixed derivative left
+    system_matrix_gg[1, 0] = -3
+    system_matrix_gg[1, 1] = 4
+    system_matrix_gg[1, 2] = -1
+    q_g[1] = 0
+    # Fixed displacement right
+    system_matrix_gg[-1] = 0
+    system_matrix_gg[-1, -1] = 1
+    q_g[-1] = 0
+    # Fixed derivative right
+    system_matrix_gg[-2, -1] = -3
+    system_matrix_gg[-2, -2] = 4
+    system_matrix_gg[-2, -3] = -1
+    q_g[-2] = 0
+
+    np.set_printoptions(precision=2, suppress=True)
+    print(system_matrix_gg)
+
+    # Solve system
     w_g = np.linalg.solve(system_matrix_gg, q_g)
 
     return w_g
--- tests/test_beam.py
+++ tests/test_beam.py
@@ -1,12 +1,22 @@
 import numpy as np
 
 from VascularFlow.Elasticity.Beam import euler_bernoulli
+from VascularFlow.Numerics.BasisFunctions import QuadraticBasis
 
 
-def test_euler_bernoulli_constant_load():
-    x_g = np.linspace(0, 1, 21)
+def test_euler_bernoulli_constant_load(plot=True):
+    left = 0
+    right = 1
+    x_g = np.linspace(left, right, 7)
     x_n = x_g[::2]
     p_g = np.ones_like(x_g)
     w_g = euler_bernoulli(x_n, p_g)
 
-    print(w_g)
+    if plot:
+        import matplotlib.pyplot as plt
+        x = np.linspace(left, right, 101)
+        plt.plot(x_g, w_g, 'kx')
+        # plt.plot(x, w_g.dot(QuadraticBasis().eval(x_g)))
+        plt.xlabel('x')
+        plt.ylabel('w')
+        plt.show()

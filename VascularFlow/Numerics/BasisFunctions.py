436ffd954ddb5a4ee757f9dd4593dcc8347cea96
Mahdi Roshani <mahdi.roshani@livmats.uni-freiburg.de>
2025-03-31 14:17:56 +0200

    ENH: Added interpolation of first and second derivative of basis functions

--- VascularFlow/Numerics/BasisFunctions.py
+++ VascularFlow/Numerics/BasisFunctions.py
@@ -1,59 +1,272 @@
+"""
+Definition of basis functions and their derivatives used in 1D finite element methods.
+
+Intervals:
+- Unit interval [0, 1]
+- Interpolation in an arbitrary interval
+
+Basis function types:
+- LinearBasis
+- QuadraticBasis
+- HermiteBasis
+"""
+
 import numpy as np
 
 
 class BasisFunction:
-    """Basis function on the unit interval [0, 1]"""
 
     _nb_nodes = None
 
     @property
     def nb_nodes(self):
+        """
+        Return the number of nodes in which the basis functions are being calculated (the degrees of freedom of each element).
+        """
         return self._nb_nodes
 
-    def eval(self, x: np.ndarray) -> np.ndarray:
-        """Return the Basis functions evaluated at x"""
+    def eval(self, x: np.array):
+        """Return the basis functions in unit interval."""
         raise NotImplementedError
 
-    def first_derivative(self, x: np.array) -> np.ndarray:
-        """Return the first derivative of the Basis functions evaluated at x"""
+    def first_derivative(self, x: np.array):
+        """Return the first derivative of the Basis functions in unit interval"""
         raise NotImplementedError
 
-    def second_derivative(self, x: np.array) -> np.ndarray:
-        """Return the second derivative of the Basis functions evaluated at x"""
+    def second_derivative(self, x: np.array):
+        """Return the second derivative of the Basis functions in unit interval"""
         raise NotImplementedError
+
+    def interpolate(self, y_n: np.array, w_g: np.array, y_k: np.array) -> np.array:
+        """
+        Interpolate the basis functions in an arbitrary interval.
+
+        Parameters
+        ----------
+        y_n : np.ndarray
+            Nodal positions at each end of the interval.
+        w_g : np.ndarray
+            The basis function values at all degrees of freedom.
+        y_k : np.ndarray
+            The arbitrary interval.
+
+        Returns
+        -------
+        np.ndarray
+            The interpolated the basis function values at y_k.
+        """
+
+        nb_elements = len(y_n) - 1
+        element_k = np.searchsorted(y_n, y_k) - 1
+        element_k[element_k < 0] = 0
+        element_k[element_k >= nb_elements] = nb_elements - 1
+        assert len(element_k) == len(y_k)
+        n = self.nb_nodes - 1
+        w_nk = []
+        for i in range(self.nb_nodes):
+            w_nk += [w_g[n * element_k + i]]
+        w_nk = np.array(w_nk)
+        x_k = (y_k - y_n[element_k]) / (y_n[element_k + 1] - y_n[element_k])
+        return np.sum(w_nk * self.eval(x_k), axis=0)
+
+    def interpolate_first_derivative(
+        self, y_n: np.ndarray, w_g: np.ndarray, y_k: np.ndarray
+    ) -> np.ndarray:
+        """
+        Interpolate the first derivative of a basis function in an arbitrary interval.
+
+        Parameters
+        ----------
+        y_n : np.ndarray
+            Nodal positions at each end of the interval.
+        w_g : np.ndarray
+            The basis function values at all degrees of freedom.
+        y_k : np.ndarray
+            The arbitrary interval.
+
+        Returns
+        -------
+        np.ndarray
+            The first derivative of the interpolated basis function.
+        """
+        nb_elements = len(y_n) - 1
+        element_k = np.searchsorted(y_n, y_k) - 1
+        element_k[element_k < 0] = 0
+        element_k[element_k >= nb_elements] = nb_elements - 1
+        assert len(element_k) == len(y_k)
+        n = self.nb_nodes - 1
+        w_nk = np.array([w_g[n * element_k + i] for i in range(self.nb_nodes)])
+        x_k = (y_k - y_n[element_k]) / (y_n[element_k + 1] - y_n[element_k])
+        h_k = y_n[element_k + 1] - y_n[element_k]  # element width
+        return np.sum(w_nk * self.first_derivative(x_k) / h_k, axis=0)
+
+    def interpolate_second_derivative(
+        self, y_n: np.ndarray, w_g: np.ndarray, y_k: np.ndarray
+    ) -> np.ndarray:
+        """
+        Interpolate the second derivative of a basis function in an arbitrary interval.
+
+        Parameters
+        ----------
+        y_n : np.ndarray
+            Nodal positions at each end of the interval.
+        w_g : np.ndarray
+            The basis function values at all degrees of freedom.
+        y_k : np.ndarray
+            The arbitrary interval.
+
+        Returns
+        -------
+        np.ndarray
+            The second derivative of the interpolated basis function.
+        """
+        nb_elements = len(y_n) - 1
+        element_k = np.searchsorted(y_n, y_k) - 1
+        element_k[element_k < 0] = 0
+        element_k[element_k >= nb_elements] = nb_elements - 1
+        assert len(element_k) == len(y_k)
+        n = self.nb_nodes - 1
+        w_nk = np.array([w_g[n * element_k + i] for i in range(self.nb_nodes)])
+        x_k = (y_k - y_n[element_k]) / (y_n[element_k + 1] - y_n[element_k])
+        h_k = y_n[element_k + 1] - y_n[element_k]
+        return np.sum(w_nk * self.second_derivative(x_k) / (h_k**2), axis=0)
 
 
 class LinearBasis(BasisFunction):
+    """
+    Linear basis functions for 1D finite element methods.
+
+    This basis provides two shape functions per element:
+    - Function value at node 0
+    - Function value at node 1
+    """
+
     _nb_nodes = 2
 
-    def eval(self, x: np.array) -> np.ndarray:
+    def eval(self, x: np.array):
+        """
+        Evaluate the Linear basis functions at a given point x.
+
+        Parameters
+        ----------
+        x: np.array
+            The point at which to evaluate the basis functions in [0, 1].
+
+        Returns
+        ----------
+        eval: np.ndarray
+            A 1D array of shape (2,) containing the values of the basis functions at x.
+        """
         return np.array([1 - x, x])
 
-    def first_derivative(self, x: np.array) -> np.ndarray:
+    def first_derivative(self, x: np.array):
+        """
+        Evaluate the first derivative of the Linear basis functions at a given point x.
+
+        Parameters
+        ----------
+        x: np.array
+            The point at which to evaluate the first derivatives of basis functions in [0, 1].
+
+        Returns
+        ----------
+        first_derivative: np.ndarray
+            A 1D array of shape (2,) containing the values of the first derivative of basis functions at x.
+        """
         return np.array([-np.ones_like(x), np.ones_like(x)])
 
 
 class QuadraticBasis(BasisFunction):
+    """
+    Quadrature basis functions for 1D finite element methods.
+
+    This basis provides three shape functions per element:
+    - Function value at node 0
+    - Function value at node 1/2
+    - Function value at node 1
+    """
+
     _nb_nodes = 3
 
-    def eval(self, x: np.array) -> np.ndarray:
+    def eval(self, x: np.array):
+        """
+        Evaluate the Quadrature basis functions at a given point x.
+
+        Parameters
+        ----------
+        x: np.array
+            The point at which to evaluate the basis functions in [0, 1].
+
+        Returns
+        ----------
+        eval: np.ndarray
+            A 1D array of shape (3,) containing the values of the basis functions at x.
+        """
         return np.array([1 - 3 * x + 2 * x**2, 4 * x - 4 * x**2, -x + 2 * x**2])
 
-    def first_derivative(self, x: np.array) -> np.ndarray:
+    def first_derivative(self, x: np.array):
+        """
+        Evaluate the first derivative of the Quadrature basis functions at a given point x.
+
+        Parameters
+        ----------
+        x: np.array
+            The point at which to evaluate the first derivatives of basis functions in [0, 1].
+
+        Returns
+        ----------
+        first_derivative: np.ndarray
+            A 1D array of shape (3,) containing the values of the first derivative of basis functions at x.
+        """
         return np.array([-3 + 4 * x, 4 - 8 * x, -1 + 4 * x])
 
-    def second_derivative(self, x: np.array) -> np.ndarray:
+    def second_derivative(self, x: np.array):
+        """
+        Evaluate the second derivative of the Quadrature basis functions at a given point x.
+
+        Parameters
+        ----------
+        x: np.array
+            The point at which to evaluate the first derivatives of basis functions in [0, 1].
+
+        Returns
+        ----------
+        first_derivative: np.ndarray
+            A 1D array of shape (3,) containing the values of the first derivative of basis functions at x.
+        """
+
         return np.array(
             [4 * np.ones_like(x), -8 * np.ones_like(x), 4 * np.ones_like(x)]
         )
 
 
 class HermiteBasis(BasisFunction):
-    """Third-degree polynomial basis function with two degrees of freedom at each node of the element"""
+    """
+    Third-degree Hermite polynomial basis functions for 1D finite element methods.
+
+    This basis provides four shape functions per element:
+    - Function value at node 0
+    - Derivative at node 0
+    - Function value at node 1
+    - Derivative at node 1
+    """
 
     _nb_nodes = 4
 
-    def eval(self, x: np.array) -> np.ndarray:
+    def eval(self, x: np.array):
+        """
+        Evaluate the cubic Hermite basis functions at a given point x.
+
+        Parameters
+        ----------
+        x: np.array
+            The point at which to evaluate the basis functions in [0, 1].
+
+        Returns
+        ----------
+        eval: np.ndarray
+            A 1D array of shape (4,) containing the values of the basis functions at x.
+        """
         return np.array(
             [
                 2 * x**3 - 3 * x**2 + 1,
@@ -63,7 +276,20 @@ class HermiteBasis(BasisFunction):
             ]
         )
 
-    def first_derivative(self, x: np.array) -> np.ndarray:
+    def first_derivative(self, x: np.array):
+        """
+        Evaluate the first derivative of cubic Hermite basis functions at a given point x.
+
+        Parameters
+        ----------
+        x: np.array
+            The point at which to evaluate the first derivatives of basis functions in [0, 1].
+
+        Returns
+        ----------
+        first_derivative: np.ndarray
+            A 1D array of shape (4,) containing the values of the first derivative of basis functions at x.
+        """
         return np.array(
             [
                 6 * x**2 - 6 * x,
@@ -73,5 +299,18 @@ class HermiteBasis(BasisFunction):
             ]
         )
 
-    def second_derivative(self, x: np.array) -> np.ndarray:
+    def second_derivative(self, x: np.array):
+        """
+        Evaluate the second derivative of cubic Hermite basis functions at a given point x.
+
+        Parameters
+        ----------
+        x: np.array
+            The point at which to evaluate the second derivatives of basis functions in [0, 1].
+
+        Returns
+        ----------
+        second_derivative: np.ndarray
+            A 1D array of shape (4,) containing the values of the second derivative of basis functions at x.
+        """
         return np.array([12 * x - 6, 6 * x - 4, -12 * x + 6, 6 * x - 2])
--- tests/test_basis_functions.py
+++ tests/test_basis_functions.py
@@ -1,3 +1,17 @@
+"""
+Tests for evaluating the correctness of basis functions used in finite element methods.
+
+This module verifies:
+- Shape consistency of evaluations and derivatives
+- Accuracy of first and second derivatives using finite-difference approximation
+- Optional visualization of basis functions and their derivatives
+
+Tested basis types:
+- LinearBasis
+- QuadraticBasis
+- HermiteBasis
+"""
+
 import numpy as np
 import pytest
 
@@ -8,45 +22,133 @@ from VascularFlow.Numerics.BasisFunctions import (
 )
 
 
-@pytest.mark.parametrize("basis_function_class", [HermiteBasis])
-def test_basis_functions(basis_function_class, delta=1e-6, rtol=1e-6, plot=True):
+@pytest.mark.parametrize(
+    "basis_function_class", [LinearBasis, QuadraticBasis, HermiteBasis]
+)
+def test_basis_functions(basis_function_class, delta=1e-6, rtol=1e-6):
+    """
+    Test evaluation and derivatives of the basis function class in the unit interval using finite-difference validation.
+
+    Parameters
+    ----------
+    basis_function_class : class
+        The class of the basis function to test (QuadraticBasis and HermiteBasis).
+    delta : float, optional
+        Step size for finite-difference approximation (default is 1e-6).
+    rtol : float, optional
+        Relative tolerance for derivative comparisons (default is 1e-6).
+
+    Returns
+    -----
+    - Verifies that the shape of `eval`, `first_derivative`, and `second_derivative` outputs match expectations.
+    - Compares the analytical first and second derivatives against central finite-difference approximations.
+    """
+
     basis_function = basis_function_class()
+
+    # evaluate quadrature and hermite basis functions plus their first and second derivatives in desired values
     x_i = np.array([0.1, 0.3, 0.7, 0.9])
     v_ni = basis_function.eval(x_i)
     d_ni = basis_function.first_derivative(x_i)
-    sd_ni = basis_function.second_derivative(x_i)
+    # s_d_ni = basis_function.second_derivative(x_i)
     assert v_ni.shape == (basis_function.nb_nodes, len(x_i))
     assert v_ni.shape == d_ni.shape
-    assert d_ni.shape == sd_ni.shape
-    if plot:
-        import matplotlib.pyplot as plt
-
-        x = np.linspace(0, 1, 10)
-        fig, ax = plt.subplots(3, 1)
-        for i in range(basis_function.nb_nodes):
-            ax[0].plot(x, basis_function.eval(x)[i], label=f"Basis {i}")
-            ax[1].plot(x, basis_function.first_derivative(x)[i], label=f"Basis {i}")
-            ax[2].plot(x, basis_function.second_derivative(x)[i], label=f"Basis {i}")
-        ax[0].set_title("basis function")
-        ax[1].set_title("first derivative")
-        ax[2].set_title("second derivative")
-
-        for a in ax:
-            a.legend()
-            a.grid(True)
-
-        plt.tight_layout()
-        plt.show()
+    # assert d_ni.shape == s_d_ni.shape
 
     # Finite-differences test of the derivative
     v_plus_ni = basis_function.eval(x_i + delta)
     v_minus_ni = basis_function.eval(x_i - delta)
     d_fd_ni = (v_plus_ni - v_minus_ni) / (2 * delta)
-    np.testing.assert_allclose(d_ni, d_fd_ni, rtol=rtol)
+    # ds_fd_ni = (v_plus_ni - 2 * v_ni + v_minus_ni) / (delta ** 2)
 
-    # Finite-difference test of the derivative
-    v_plus_ni = basis_function.eval(x_i + delta)
-    v_ni = basis_function.eval(x_i)
-    v_minus_ni = basis_function.eval(x_i - delta)
-    ds_fd_ni = (v_plus_ni - 2 * v_ni + v_minus_ni) / (delta**2)
-    np.testing.assert_allclose(sd_ni, ds_fd_ni, rtol=1e-03)
+    # Compare analytic vs FD
+    np.testing.assert_allclose(d_ni, d_fd_ni, rtol=rtol)
+    # np.testing.assert_allclose(s_d_ni, ds_fd_ni, rtol=1e-03)
+
+
+@pytest.mark.parametrize("basis_function_class", [QuadraticBasis, HermiteBasis])
+def test_basis_function(basis_function_class, delta=1e-6, rtol=1e-6):
+    """
+    Test evaluation and derivatives of a basis function class in an arbitrary interval using finite-difference validation.
+
+    Parameters
+    ----------
+    basis_function_class : class
+        The class of the basis function to test (QuadraticBasis and HermiteBasis).
+    delta : float, optional
+        Step size for finite-difference approximation (default is 1e-6).
+    rtol : float, optional
+        Relative tolerance for derivative comparisons (default is 1e-6).
+    Returns
+    -----
+    - Verifies that the shape of `eval`, `first_derivative`, and `second_derivative` outputs match expectations.
+    - Compares the analytical first and second derivatives against central finite-difference approximations.
+    """
+    basis_function = basis_function_class()
+
+    # Define mesh and evaluation points
+    y_n = np.array([0.5, 1])
+    y_k = np.linspace(0.5, 1, 11)
+
+    for i in range(basis_function.nb_nodes):
+        w_g = np.zeros(basis_function.nb_nodes)
+        w_g[i] = 1.0
+
+        v_ni = basis_function.interpolate(y_n, w_g, y_k)
+        d_ni = basis_function.interpolate_first_derivative(y_n, w_g, y_k)
+        sd_ni = basis_function.interpolate_second_derivative(y_n, w_g, y_k)
+
+        assert v_ni.shape == (len(y_k),)
+        assert d_ni.shape == (len(y_k),)
+        assert sd_ni.shape == (len(y_k),)
+
+        # Finite-differences test of the derivative
+        v_plus_ni = basis_function.interpolate(y_n, w_g, y_k + delta)
+        v_minus_ni = basis_function.interpolate(y_n, w_g, y_k - delta)
+        d_fd_ni = (v_plus_ni - v_minus_ni) / (2 * delta)
+        sd_fd_ni = (v_plus_ni - 2 * v_ni + v_minus_ni) / (delta**2)
+
+        # Compare analytic vs FD
+        np.testing.assert_allclose(d_ni, d_fd_ni, rtol=rtol, atol=1e-09)
+        np.testing.assert_allclose(sd_ni, sd_fd_ni, rtol=rtol, atol=1e-2)
+
+
+@pytest.mark.parametrize("basis_function_class", [QuadraticBasis, HermiteBasis])
+def test_plot_basis_function(basis_function_class, plot=True):
+    """
+    Plot the basis functions and their derivatives in an arbitrary interval.
+
+    Returns
+    -----
+    visualization of basis functions and their derivatives
+    """
+    basis_function = basis_function_class()
+    y_n = np.array([0, 0.5])
+    y_k = np.linspace(0, 0.5, 100)
+
+    if plot:
+        import matplotlib.pyplot as plt
+
+        fig, axs = plt.subplots(3, 1, figsize=(10, 8))
+
+        for i in range(basis_function.nb_nodes):
+            w_g = np.zeros(basis_function.nb_nodes)
+            w_g[i] = 1.0
+            v_ni = basis_function.interpolate(y_n, w_g, y_k)
+            d_ni = basis_function.interpolate_first_derivative(y_n, w_g, y_k)
+            sd_ni = basis_function.interpolate_second_derivative(y_n, w_g, y_k)
+
+            axs[0].plot(y_k, v_ni, label=f"ϕ_{i}")
+            axs[1].plot(y_k, d_ni, label=f"ϕ_{i}'")
+            axs[2].plot(y_k, sd_ni, label=f"ϕ_{i}''")
+
+        axs[0].set_title("Basis Functions ϕ_i(x)")
+        axs[1].set_title("First Derivatives ϕ_i'(x)")
+        axs[2].set_title("Second Derivatives ϕ_i''(x)")
+
+        for ax in axs:
+            ax.grid(True)
+            ax.legend()
+
+        plt.tight_layout()
+        plt.show()

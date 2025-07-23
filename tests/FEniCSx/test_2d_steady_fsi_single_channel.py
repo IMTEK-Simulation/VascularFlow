1adae865c5ecea3503507a8b49c6b4528518e728
Mahdi Roshani <mahdi.roshani@livmats.uni-freiburg.de>
2025-07-23 12:30:20 +0200

    TST: test for deformation of the beam using  "linear" "moderate" "nonlinear" to compare the max deformation of the beam in the single channel.

--- tests/FEniCSx/test_2d_steady_fsi_single_channel.py
+++ tests/FEniCSx/test_2d_steady_fsi_single_channel.py
@@ -20,7 +20,7 @@ from VascularFlow.FEniCSx.PostProcessing.VisualizeMesh import visualize_mesh
     ),
     [
         # Format: (L, nx, ny, Re, p_in, B, A, relax, tol, max_iter)
-        (50, 500, 10, 1.0, 280.0, 9e4, 1.6e5, 0.0003, 1e-4, 2000),
+        (50, 500, 10, 7.5, 37.3, 1007, 12088, 0.0003, 1e-4, 2000),
     ],
 )
 
@@ -77,24 +77,35 @@ def test_steady_state_fsi_single_channel_parametrized(
     assert max_displacement > 0.0, "Displacement should not be zero under load."
     assert max_pressure > 0.0, "Pressure must remain positive at the interface."
 
+    #dx = abs(fluid_solid_domain_inlet_coordinate - fluid_solid_domain_outlet_coordinate) / nb_mesh_nodes_x
+    #dy = np.diff(displacement)
+    #theta = np.arctan2(dy, dx)  # theta will be in radians
+    #max_theta = np.max(np.abs(theta))
+    #print(f"Max rotation degree in radians : {max_theta:.4f}")
+
     # Optional visualization (toggle for local use)
     visualize = True
     if visualize:
         visualize_mesh(deformed_mesh, title="Deformed Mesh")
 
-        fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(10, 6))
+        fig, ax = plt.subplots(nrows=3, ncols=1, figsize=(14, 11))
         x = np.linspace(0, 50, nb_mesh_nodes_x + 1)
         # Plots
-        ax[0].plot(x, pressure)
-        ax[1].semilogy(iteration_indices[1:], residual_values[1:])
+        ax[0].plot(x, displacement, color="green")
+        ax[1].plot(x, pressure, color="blue")
+        ax[2].semilogy(iteration_indices[1:], residual_values[1:], color="orange")
         # Set labels
-        ax[0].set_xlabel("Normalized Channel Length (x)")
-        ax[0].set_ylabel("Normalized Channel Pressure")
-        ax[1].set_xlabel("Cumulative Iteration Count")
-        ax[1].set_ylabel("Residual (log scale)")
+        ax[0].set_xlabel("Normalized distance\n$\\tilde{x} = \\dfrac{x}{H_0}$")
+        ax[0].set_ylabel("Normalized displacement\n$\\tilde{w} = \\dfrac{w}{H_0}$")
+        ax[1].set_xlabel("Normalized distance\n$\\tilde{x} = \\dfrac{x}{H_0}$")
+        ax[1].set_ylabel("Normalized Pressure \n$\\tilde{p} = \\dfrac{p_{\\mathrm{inlet}}}{\\rho U^2}$")
+        ax[2].set_xlabel("Cumulative Iteration Count")
+        ax[2].set_ylabel("Residual (log scale)")
         # Enable grid on both subplots
         ax[0].grid(True)
         ax[1].grid(True)
+        # Set the figure title
+        fig.suptitle("FSI single Channel Results")
         plt.tight_layout()
         plt.savefig("single channel.png")
         plt.show()

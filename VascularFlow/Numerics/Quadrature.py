85af0edd4586017ed2b7854dff2b40331b409c67
Mahdi Roshani <mahdi.roshani@livmats.uni-freiburg.de>
2025-08-15 16:42:04 +0200

    ENH: add 2D Gaussian quadrature rules for reference square. Implemented 2D quadrature points and weights for 4- and 9-point scheme

--- VascularFlow/Numerics/Quadrature.py
+++ VascularFlow/Numerics/Quadrature.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+########################################################## 1D ##########################################################
+
 _quadrature_points = {
     1: np.array([0]),
     2: np.array([-1 / np.sqrt(3), 1 / np.sqrt(3)]),
@@ -69,3 +71,82 @@ def gaussian_quadrature(nb_quad_pts: int, a: float, b: float, func: callable) ->
             nb_quad_pts, lambda x: func(0.5 * (b - a) * x + 0.5 * (b + a))
         )
     )
+
+
+########################################################## 2D ##########################################################
+
+_quadrature_points_2D = {
+    4: np.array(
+        [
+            [-np.sqrt(1 / 3), -np.sqrt(1 / 3)],
+            [np.sqrt(1 / 3), -np.sqrt(1 / 3)],
+            [np.sqrt(1 / 3), np.sqrt(1 / 3)],
+            [-np.sqrt(1 / 3), np.sqrt(1 / 3)],
+        ]
+    ),
+    9: np.array(
+        [
+            [0,                         0],
+            [-np.sqrt(0.6), -np.sqrt(0.6)],
+            [-np.sqrt(0.6),  np.sqrt(0.6)],
+            [np.sqrt(0.6),   np.sqrt(0.6)],
+            [np.sqrt(0.6),  -np.sqrt(0.6)],
+            [-np.sqrt(0.6),             0],
+            [0,              np.sqrt(0.6)],
+            [np.sqrt(0.6),              0],
+            [0,             -np.sqrt(0.6)],
+        ]
+    ),
+}
+
+_quadrature_weights_2D = {
+    4: np.array(
+        [
+            1,
+            1,
+            1,
+            1,
+        ]
+    ),
+    9: np.array(
+        [
+            (8 / 9) ** 2,
+            (5 / 9) ** 2,
+            (5 / 9) ** 2,
+            (5 / 9) ** 2,
+            (5 / 9) ** 2,
+            (5 / 9) * (8 / 9),
+            (5 / 9) * (8 / 9),
+            (5 / 9) * (8 / 9),
+            (5 / 9) * (8 / 9),
+        ]
+    ),
+}
+
+
+
+def integrate_over_square(func: callable, nb_quad_pts_2d: int) -> float:
+    """
+    Integrate a 2D function f(ξ, η) over the reference square [-1, 1] × [-1, 1]
+    using 2D Gaussian quadrature.
+
+    Parameters
+    ----------
+    func : callable
+        A scalar function f(ξ, η) to integrate. Should accept two float arguments (ξ, η).
+    nb_quad_pts_2d : int, optional
+        Number of quadrature points (supported: 4 or 9). Default is 9.
+
+    Returns
+    -------
+    float
+        Approximate integral value using 2D Gaussian quadrature.
+    """
+    gauss_points = _quadrature_points_2D[nb_quad_pts_2d]
+    weights = _quadrature_weights_2D[nb_quad_pts_2d]
+
+    integral = 0.0
+    for (s, n), w in zip(gauss_points, weights):
+        integral += w * func(s, n)
+
+    return integral
